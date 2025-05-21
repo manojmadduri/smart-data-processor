864c9514d401093605a44782246669baeb0ef424
Manoj <121824103+manojmadduri@users.noreply.github.com>
2025-05-21 18:32:02 -0400

    Update ProgressSpinner.jsx

--- frontend/src/components/ProgressSpinner.jsx
+++ frontend/src/components/ProgressSpinner.jsx
@@ -3,13 +3,8 @@ import React from 'react';
 export default function ProgressSpinner() {
   return (
 <div
-  className="
-    w-10 h-10 
-    border-4 border-gray-300 
-    border-t-white 
-    rounded-full 
-    animate-spin
-  "
+  className="spinner w-10 h-10 border-4 border-gray-300 border-t-white rounded-full"
 />
+
   );
 }
