7e7977fe66fa859b75d293b521b684fb5b24d2fd
Manoj <121824103+manojmadduri@users.noreply.github.com>
2025-05-24 17:51:29 -0400

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -25,9 +25,6 @@ export default function NavBar() {
           >
             Upload
           </Link>
-                    <Link to="/guide" className={navLinkClass('/guide')}>
-            FineTuning Guide
-          </Link>
 
           <button
             onClick={toggle}
