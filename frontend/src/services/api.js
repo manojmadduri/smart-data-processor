570ced005a7490011bdbc1ea61a27613ad04d3f9
Manoj <121824103+manojmadduri@users.noreply.github.com>
2025-05-29 16:11:02 -0400

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_ROOT = process.env.REACT_APP_API_URL || 'https://smart-data-processor.onrender.com';
+const BACKEND_ROOT = process.env.REACT_APP_API_URL || 'https://smart-data-processor-1.onrender.com/';
 
 export const api = axios.create({
   baseURL: `${BACKEND_ROOT}/api`,
